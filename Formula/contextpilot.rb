9895fa94c09445d86e8e4bcf94f3704e94a8aca9
Jitin Gambhir <jitingambhir9@gmail.com>
2026-02-17 00:12:02 +0530

    Update formula with v0.1.0 checksums

--- Formula/contextpilot.rb
+++ Formula/contextpilot.rb
@@ -11,22 +11,22 @@ class Contextpilot < Formula
   on_macos do
     on_intel do
       url "https://github.com/contextpilot-dev/contextpilot/releases/download/v#{version}/contextpilot-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+      sha256 "c3e0c4d94713291f46b98c81964892871dbb12dd2b171f503b45b4c3ba1e227b"
     end
     on_arm do
       url "https://github.com/contextpilot-dev/contextpilot/releases/download/v#{version}/contextpilot-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      sha256 "c00db5899c973130fb1d1df7f380212dcc323c160295992f43ae9f9821644429"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/contextpilot-dev/contextpilot/releases/download/v#{version}/contextpilot-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      sha256 "739fb55136bebf5aa2ce807998f74f84980af097d61561d34f7318fad2a5e8dc"
     end
     on_arm do
       url "https://github.com/contextpilot-dev/contextpilot/releases/download/v#{version}/contextpilot-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "f178ab306159b9cebbfb820e28d7aeb1bf225202b965c15d825e4f5cb0a45436"
     end
   end
 
